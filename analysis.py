24f6640b4f621a212fa0436b5a875da2cbe30ee7
dukim <sajacall82@naver.com>
2023-09-15 18:55:04 +0900

    feat: print hello message with web
    web에 hello world 메시지 출력

--- analysis.py
+++ analysis.py
@@ -1,6 +1,12 @@
+import plotly
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import dash_bootstrap_components as dbc
 
+external_stylesheets = [dbc.themes.FLATLY]  # CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
-
+app.layout = dbc.Container([html.H4('hello world')])
 
 if __name__=='__main__':
-    print('helloworld')
+    app.run(debug=True)
