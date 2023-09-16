34bb37697658e94b3cce24f3c2e78e6d3d923f9e
dukim <sajacall82@naver.com>
2023-09-16 19:03:11 +0900

    feat: apply 버튼 콜백 완료
    input - button
    state - radio button, input box/select box
    output - 하단 div

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 import plotly
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import seaborn as sns
@@ -15,11 +15,34 @@ def csv_input(disable=False):
         otherwise, input enable
         default False
     :return:
-        csv url by user input
+        input box for user input
     """
-    return html.Div([
-        dbc.Input(id='csv_input', placeholder='Type csv url', type='text', disabled=disable),
-    ])
+    return dbc.Input(id='data_name_select',type='text', value='https://raw.githubusercontent.com/DSNote/fastcampus/main/rent.csv', disabled=disable)
+
+
+def select_input(data_source, disable=False):
+    """
+    :param data_source:
+        data source name, seaborn, plotly, sklearn
+    :param disable:
+        if True, input disable
+        otherwise, input enable
+        default False
+    :return:
+        select box for user input
+    """
+    data_list = (
+        seaborn_datalist() if data_source == 'seaborn' else
+        plotly_datalist() if data_source == 'plotly' else
+        sklearn_datalist()
+    )
+
+    return dbc.Select(
+        id='data_name_select',
+        options=[{'label': data, 'value': data} for data in data_list],
+        value=data_list[0],
+        disabled=disable,
+    )
 
 
 def seaborn_datalist():
@@ -56,36 +79,33 @@ def datasource_input(data_source, disable=False):
         csv, seaborn, plotly, sklearn
     :param disable:
         if True, select disable
-        otherwise, sleect enable
+        otherwise, select enable
         default False
     :return:
         select input for data_source
     """
-    data_list = (
-        seaborn_datalist() if data_source == 'seaborn' else
-        plotly_datalist() if data_source == 'plotly' else
-        sklearn_datalist()
-    )
-    return html.Div([
-        csv_input(disable) if data_source == 'csv' else
-        dbc.Select(
-            id=data_source + '_input',
-            options=[{'label': data, 'value': data} for data in data_list],
-            value=data_list[0],
-            disabled=disable,
-        ),
-    ])
+    return csv_input(disable) if data_source == 'csv' else select_input(data_source, disable)
 
 input_types = ['csv', 'seaborn', 'plotly', 'sklearn']
 @app.callback(
-    Output(component_id='user_input', component_property='children'),
-    Input(component_id='user_input_select', component_property='value'),
+    Output(component_id='user_input_component', component_property='children'),
+    Input(component_id='datasource_select', component_property='value'),
 )
 def activate_input_component(target):
-    activates = [not(target == t) for t in input_types]
-    return [
-        dbc.Row(datasource_input(t, activate)) for t, activate in zip(input_types, activates)
-    ]
+    return dbc.Row(datasource_input(target))
+
+@app.callback(
+    Output(component_id='analysis_area', component_property='children'),
+    Input(component_id='apply_submit', component_property='n_clicks'),
+    State(component_id='datasource_select', component_property='value'),
+    State(component_id='data_name_select', component_property='value'),
+    prevent_initial_call=True,
+)
+def apply_datasource(n_clicks, datasource, data_name):
+    # data_df = get_data(datasource)
+    # data_df.head()
+    print(datasource)
+    return html.Div(f'{datasource}, {data_name}')
 
 
 app.layout = dbc.Container([
@@ -93,7 +113,7 @@ app.layout = dbc.Container([
         dbc.Row([
             dbc.Col(
                 dbc.RadioItems(
-                    id='user_input_select',
+                    id='datasource_select',
                     options={
                         'csv': 'csv url',
                         'seaborn': 'seaborn data',
@@ -103,10 +123,10 @@ app.layout = dbc.Container([
                     value='csv',
                 ),
             ),
-            dbc.Col(html.Div(id='user_input')),
-            dbc.Col(dbc.Button('apply', color='primary', className='me-1')),
+            dbc.Col(html.Div(id='user_input_component')),
+            dbc.Col(dbc.Button(id='apply_submit', children='apply', n_clicks=0, color='primary', className='me-1')),
         ]),
-        dbc.Row(html.Div('chart'), style={'backgroundColor': 'yellow'}),
+        dbc.Row(html.Div(id='analysis_area'), style={'backgroundColor': 'yellow'}),
     ]),
 ])
 
