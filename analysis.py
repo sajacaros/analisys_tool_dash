949913ccf802fc530567e6d2bed6668fe2190e70
dukim <sajacall82@naver.com>
2023-09-16 14:23:27 +0900

    feat: text input ui 추가
    text input ui 추가

--- analysis.py
+++ analysis.py
@@ -6,6 +6,12 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.FLATLY]  # CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
+
+def csv_input():
+    return html.Div([
+        dbc.Input(id='csv_input', placeholder='Type csv url', type='text'),
+    ])
+
 app.layout = dbc.Container([
     dbc.Container([
         dbc.Row([
@@ -20,7 +26,12 @@ app.layout = dbc.Container([
                     value='csv',
                 ),
             ),
-            dbc.Col(html.Div('world'), style={'backgroundColor': 'red'}),
+            dbc.Col([
+                dbc.Row(csv_input()),
+                dbc.Row(html.Div('hello')),
+                dbc.Row(html.Div('hello')),
+                dbc.Row(html.Div('hello')),
+            ]),
             dbc.Col(html.Div('!!!'), style={'backgroundColor': 'blue'}),
         ]),
         dbc.Row(html.Div('chart'), style={'backgroundColor': 'yellow'}),
