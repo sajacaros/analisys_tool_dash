7dc8a07429947a8ae23cea8340e1334b62ba2566
dukim <sajacall82@naver.com>
2023-09-16 15:08:37 +0900

    feat: select 목록 추가
    select 목록
    - seaborn
    - plotly
    - sklearn

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import plotly
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import seaborn as sns
+
 
 external_stylesheets = [dbc.themes.FLATLY]  # CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
@@ -11,6 +13,50 @@ def csv_input():
     return html.Div([
         dbc.Input(id='csv_input', placeholder='Type csv url', type='text'),
     ])
+
+def seaborn_datalist():
+    """
+    :return:
+        seaborn data list
+    """
+    return sns.get_dataset_names()
+
+def plotly_datalist():
+    """
+    :return:
+        plotly data list
+    """
+    return ['carshare', 'election', 'experiment', 'gapminder', 'iris', 'medals_long', 'medals_wide', 'stocks', 'tips', 'wind']
+
+def sklearn_datalist():
+    """
+    :return:
+        sklearn data list
+    """
+    return ['iris', 'diabetes', 'digits', 'wine', 'breast_cancer']
+
+def select_input(data_source):
+    """
+    :param data_source:
+        seaborn, plotly, sklearn
+    :return:
+        select input for data_source
+    """
+    data_list = (
+        seaborn_datalist() if data_source == 'seaborn' else
+        plotly_datalist() if data_source == 'plotly' else
+        sklearn_datalist()
+    )
+    print(data_source)
+    print([{data: data} for data in data_list])
+    return html.Div([
+        dbc.Select(
+            id=data_source+'_input',
+            options=[{'label': data, 'value': data} for data in data_list],
+            value=data_list[0],
+        ),
+    ])
+
 
 app.layout = dbc.Container([
     dbc.Container([
@@ -28,9 +74,9 @@ app.layout = dbc.Container([
             ),
             dbc.Col([
                 dbc.Row(csv_input()),
-                dbc.Row(html.Div('hello')),
-                dbc.Row(html.Div('hello')),
-                dbc.Row(html.Div('hello')),
+                dbc.Row(select_input('seaborn')),
+                dbc.Row(select_input('plotly')),
+                dbc.Row(select_input('sklearn')),
             ]),
             dbc.Col(html.Div('!!!'), style={'backgroundColor': 'blue'}),
         ]),
